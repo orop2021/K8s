2f0fe00e2d05b45fa2abaca97a80b64f9c0c8a9e
root <root@ip-172-31-89-70.ec2.internal>
2022-06-19 20:31:42 +0000

    New Changes

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "k8_instance" {
       ami = var.ami
       key_name = var.key_name
       instance_type = var.instance_type
-      group_id = ["sg-02695d387645798f6"]
+      security_group_id = ["sg-02695d387645798f6"]
       tags = {
          Name = var.name
          env = var.env
